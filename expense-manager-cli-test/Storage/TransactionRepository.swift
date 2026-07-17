280087ae346a75e8dc0262b37030ce45b0a661f0
beispresheva <beis.presheva@riinvest.net>
2026-07-17 15:31:24 +0200

    - added TransactionRepository

---
+++ expense-manager-cli-test/Storage/TransactionRepository.swift
@@ -0,0 +1,40 @@
+//
+//  TransactionRepository.swift
+//  expense-manager-cli-test
+//
+//  Created by Beis Presheva on 17.7.26.
+//
+
+import Foundation
+
+final class TransactionRepository {
+    private var transactions: [Transaction] = []
+    var count: Int {
+        return transactions.count
+    }
+    
+    // functions
+    
+    func add(_ transaction: Transaction) {
+        transactions.append(transaction)
+    }
+    
+    func getAll() -> [Transaction] {
+        return transactions
+    }
+    
+    func findById(by id: UUID) -> Transaction? {
+        return transactions.first { transaction in
+            transaction.id == id
+        }
+    }
+    
+    func delete(by id: UUID) {
+        if let index = transactions.firstIndex(where: { $0.id == id }) {
+            transactions.remove(at: index)
+        }
+        else {
+            print("ID does not exist. Cannot delete!")
+        }
+    }
+}
--- expense-manager-cli-test/main.swift
+++ expense-manager-cli-test/main.swift
@@ -7,12 +7,26 @@
 
 import Foundation
 
-let transaction = Transaction(title: "Netflix", amount: 12.99, type: .expense, category: .subscriptions)
+let repository = TransactionRepository()
 
-print("My Transaction: \n")
-print(transaction.title)
-print(transaction.amount)
-print(transaction.type.rawValue)
-print(transaction.category.rawValue)
-print(transaction.note ?? "No note provided.")
-print(transaction.merchant ?? "No merchant provided.")
+let transaction1 = Transaction(title: "Netflix", amount: 12.99, type: .expense, category: .subscriptions)
+let transaction2 = Transaction(title: "Groceries", amount: 50.99, type: .expense, category: .food)
+let transaction3 = Transaction(title: "Income from freelance", amount: 1499.99, type: .income, category: .other)
+
+repository.add(transaction1)
+repository.add(transaction2)
+repository.add(transaction3)
+
+// test deletion
+
+repository.delete(by: transaction1.id)
+
+let remainingTransactions = repository.getAll()
+
+if remainingTransactions.isEmpty {
+    print("There are not transactions in the repository")
+} else {
+    print(remainingTransactions.count)
+}
+
+
